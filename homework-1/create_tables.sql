c8c59d576fe58c2812f95f26b2c30c56e82ae33b
anzelikagudkova <kkeerrll137@gmail.com>
2023-08-24 12:45:12 +0300

    dz1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,23 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees (
+    employee_id varchar(50) PRIMARY KEY,
+    first_name varchar(50),
+    last_name varchar(50),
+    title varchar(255),
+    birth_date date,
+    notes text
+);
+
+CREATE TABLE customers (
+    customer_id varchar(50) PRIMARY KEY,
+    company_name varchar(50),
+    contact_name varchar(50)
+);
+
+CREATE TABLE orders (
+    order_id integer PRIMARY KEY,
+    customer_id varchar(50) REFERENCES customers,
+    employee_id varchar(50) REFERENCES employees,
+    order_date date,
+    ship_city varchar(50)
+);
